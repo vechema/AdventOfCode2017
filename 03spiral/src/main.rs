c0ed40168ae506938f97aef8a549df525c1aedff
Jo <vechema@gmail.com>
2018-01-15 15:45:10 -0600

    Improve day 3 spiral solution

--- 03spiral/src/main.rs
+++ 03spiral/src/main.rs
@@ -1,4 +1,7 @@
-const size : usize = 11;
+use std::ops::Index;
+use std::ops::IndexMut;
+
+const SIZE : usize = 11;
 
 fn main() {
     let input: f64 = 289326.0;
@@ -17,32 +20,53 @@ fn spiral_dist(input: f64) -> f64 {
 	((((max_dist + 1.0).powf(2.0) - input) % max_dist) - (max_dist/2.0)).abs() + max_dist/2.0
 }
 
+#[derive(Clone)]
+struct Point {
+	x: usize,
+	y: usize,
+}
+
+impl Point {
+	fn translate(&self, (x_delta, y_delta): (i32, i32)) -> Point {
+		Point{x:(self.x as i32 + x_delta) as usize, y:(self.y as i32 + y_delta) as usize}
+	}
+}
+
+impl Index<Point> for [[u32;SIZE];SIZE] {
+	type Output = u32;
+	fn index(&self, point: Point) -> &u32 {
+		&self[point.x][point.y]
+	}
+}
+
+impl IndexMut<Point> for [[u32;SIZE];SIZE] {
+	fn index_mut(&mut self, point: Point) -> &mut u32 {
+		&mut self[point.x][point.y]
+	}
+}
+
 fn sum_spiral(limit: u32) -> u32 {
 
-	let mut vec = [[0;size];size];
+	let mut vec = [[0;SIZE];SIZE];
 	let mut num = 1;
-	let mut x = size / 2;
-	let mut y = size / 2;
-	vec[x][y] = num;
-
-	let mut result = std::u32::MAX;
+	let mut loc = Point {x: SIZE / 2, y: SIZE / 2};
+	vec[loc.clone()] = num;
 
 	let movement = [(0,1),(-1,0),(0,-1),(1,0)];
 	let mut movement_index = 0;
 	let mut movement_done = false;
 	let mut movement_iterations = 1;
 
-	while num <= limit{
+	loop {
 
-		for iter in 0..movement_iterations {
-			x = move_x_coordinate(x,movement[movement_index]);
-			y = move_y_coordinate(y,movement[movement_index]);
-			num = find_surrounding_sum(x, y, &vec);
+		for _ in 0..movement_iterations {
+			loc = loc.translate(movement[movement_index]);
+			num = find_surrounding_sum(&loc, &vec);
+			vec[loc.clone()] = num;
 			if num > limit {
-				result = num;
-				break;
+				print_2d_array(&vec);
+				return num;
 			}
-			vec[x][y] = num;
 		}
 		movement_index = (movement_index + 1) % movement.len();
 		if movement_done {
@@ -52,37 +76,27 @@ fn sum_spiral(limit: u32) -> u32 {
 			movement_done = !movement_done;
 		}
 	}
-	print_2d_array(&vec);
-
-	result
-}
-
-fn move_x_coordinate(x: usize, change: (i32, i32)) -> usize {
-	let (x_delta, y_delta) = change;
-	(x as i32+ x_delta) as usize
-}
-
-fn move_y_coordinate(y: usize, change: (i32, i32)) -> usize {
-	let (x_delta, y_delta) = change;
-	(y as i32 + y_delta) as usize
 }
 
 // Gotta have a size big enough to keep an outer border
-fn find_surrounding_sum(x : usize, y : usize, vec : &[[u32; size];size]) -> u32 {
-	let rt = vec[x][y + 1];
-	let lt = vec[x][y - 1];
-	let up = vec[x - 1][y];
-	let dn = vec[x + 1][y];
-	let rt_up = vec[x - 1][y + 1];
-	let rt_dn = vec[x + 1][y + 1];
-	let lt_up = vec[x - 1][y - 1];
-	let lt_dn = vec[x + 1][y - 1];
-	vec[x][y] + rt + lt + up + dn + rt_up + rt_dn + lt_up + lt_dn
+fn find_surrounding_sum(loc: &Point, vec : &[[u32; SIZE];SIZE]) -> u32 {
+	let rt = vec[loc.translate((0,1))];
+	let lt = vec[loc.translate((0,-1))];
+	let up = vec[loc.translate((-1,0))];
+	let dn = vec[loc.translate((1,0))];
+	let rt_up = vec[loc.translate((-1,1))];
+	let rt_dn = vec[loc.translate((1,1))];
+	let lt_up = vec[loc.translate((-1,-1))];
+	let lt_dn = vec[loc.translate((1,-1))];
+	rt + lt + up + dn + rt_up + rt_dn + lt_up + lt_dn
 }
 
-fn print_2d_array(array: &[[u32; size];size]) {
+fn print_2d_array(array: &[[u32; SIZE];SIZE]) {
     for arr in array.into_iter() {
-		println!("{:?}", arr);
+		for elem in arr.iter() {
+			print!("{:6}, ", elem);
+		}
+		println!("");
 	}
 }
 
