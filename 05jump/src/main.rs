ec8ddb62ac944616e0f4e35bcfb12a3d45e43a6f
Jo <vechema@gmail.com>
2018-01-15 16:14:12 -0600

    Solve day 5 jump, part 1

--- 05jump/src/main.rs
+++ 05jump/src/main.rs
@@ -10,5 +10,31 @@ fn main() {
 		v.push(i.to_string().parse::<i32>().unwrap());
 	}
 
-	println!("{:?}", v);
+	let ans = num_steps(&v);
+	let ans = num_steps(&v);
+	println!("{:?}", ans);
+}
+
+fn num_steps(vec: &Vec<i32>) -> u32 {
+	let mut input = vec.clone();
+	let mut num_steps = 0;
+	let mut index = 0;
+	while index < input.len() {
+		let jump = input[index];
+		input[index] = jump + 1;
+		index = (index as i32 + jump) as usize;
+		num_steps+=1;
+	}
+	num_steps
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn captcha_simple_repeat() {
+		let vec = vec![0, 3, 0, 1, -3];
+		assert_eq!(num_steps(&vec), 5);
+	}
 }
