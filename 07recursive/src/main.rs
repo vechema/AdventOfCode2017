b681adbabfdfee2cf08d3d3c2ff7dfc1066030ee
Jo <vechema@gmail.com>
2018-01-26 20:44:31 -0600

    Solve day 7 recursive part 1 in a way that probably won't help part 2 at all because trees in Rust are hard

--- 07recursive/src/main.rs
+++ 07recursive/src/main.rs
@@ -1,7 +1,70 @@
 extern crate utilities;
+use std::collections::HashSet;
 
 fn main() {
 	let input = utilities::read_file("input.txt");
 
-	println!("{}",input);
+	let tower_list = build_tower_list(&input);
+	let bottom_tower = find_bottom_tower(&tower_list);
+
+	println!("{:?}", bottom_tower);
+}
+
+#[derive(Debug)]
+struct Tower {
+	name: String,
+	weight: u32,
+	kids: Option<Vec<String>>,
+}
+
+fn build_tower_list(input: &String) -> Vec<Tower> {
+
+	let mut result = Vec::new();
+
+	for line in input.split("\r\n") {
+		let mut parts = line.split(" ");
+		let name = parts.next().unwrap().to_owned();
+
+		let weight_unformatted = parts.next().unwrap();
+		let weight = weight_unformatted[1..weight_unformatted.len()-1].parse::<u32>().unwrap();
+
+		let mut kids = None;
+
+		if let Some(_) = parts.next() { // Get rid of ->
+			let unformatted_kids = parts.collect::<Vec<&str>>();
+			kids = Some(unformatted_kids.iter()
+				.map(|&x| {
+					if x.contains(",") {
+						String::from(&x[..x.len()-1])
+					} else {
+						String::from(x)
+					}
+				})
+				.collect::<Vec<String>>())
+		}
+
+		let new_tower = Tower {name, weight, kids};
+		result.push(new_tower);
+	}
+
+	result
+}
+
+fn find_bottom_tower(tower_list: &Vec<Tower>) -> String {
+	let mut all_the_chillans: HashSet<String> = HashSet::new();
+	for tower in tower_list {
+		if let Some(ref kids) = tower.kids {
+			for kid in kids {
+				all_the_chillans.insert(kid.clone());
+			}
+		}
+	}
+
+	let mut all_tower_names = HashSet::new();
+	for tower in tower_list {
+		let &Tower {ref name, ..} = tower;
+		all_tower_names.insert(name.clone());
+	}
+
+	all_tower_names.difference(&all_the_chillans).next().unwrap().clone()
 }
