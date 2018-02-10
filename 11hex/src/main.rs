06c33c9e04a9996759ad940724808b5711d4311e
Jo <vechema@gmail.com>
2018-02-09 19:16:11 -0600

    Day 11 part 1 - Parse input

--- 11hex/src/main.rs
+++ 11hex/src/main.rs
@@ -3,5 +3,34 @@ extern crate utilities;
 fn main() {
     let input = utilities::read_file("input.txt");
 
-	println!("{}", input);
+	let steps = make_direction_list(&input);
+	println!("{:?}",steps);
+}
+
+fn make_direction_list(input: &String) -> Vec<Direction> {
+
+	let mut steps = Vec::new();
+	for dir in input.split(",") {
+		let direction = match dir {
+            "n"  => Direction::N,
+            "ne" => Direction::NE,
+            "nw" => Direction::NW,
+            "s"  => Direction::S,
+            "se" => Direction::SE,
+			_    => Direction::SW,
+        };
+		steps.push(direction);
+	}
+
+	steps
+}
+
+#[derive(Debug)]
+enum Direction {
+	N,
+	NE,
+	NW,
+	S,
+	SE,
+	SW,
 }
