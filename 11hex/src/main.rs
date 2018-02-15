396e0151570350938938f5d69f3a3f01f273b74f
Jo <vechema@gmail.com>
2018-02-14 20:53:05 -0600

    Add day 11 hex helpers

--- 11hex/src/main.rs
+++ 11hex/src/main.rs
@@ -1,10 +1,13 @@
 extern crate utilities;
+use self::Direction::{N,NE,NW,S,SE,SW};
+use self::ComboDirection::{Cancel,Combo,NoCombo};
 
 fn main() {
     let input = utilities::read_file("input.txt");
 
 	let steps = make_direction_list(&input);
-	println!("{:?}",steps);
+	let compact_steps = compact_directions(&steps);
+	println!("{:?}", compact_steps);
 }
 
 fn make_direction_list(input: &String) -> Vec<Direction> {
@@ -25,6 +28,32 @@ fn make_direction_list(input: &String) -> Vec<Direction> {
 	steps
 }
 
+fn compact_directions(directions: &Vec<Direction>) -> Vec<Direction> {
+	Vec::new()
+}
+
+fn combine_directions(direction1: &Direction, direction2: &Direction) -> ComboDirection {
+	if areOpposite(direction1, direction2) {
+		return Cancel;
+	}
+	NoCombo
+}
+
+fn areOpposite(dir1: &Direction, dir2: &Direction) -> bool {
+	// N&S
+	// NE&SW
+	// NW&SE
+	match (dir1, dir2) {
+		(&N, &S) => return true,
+		(&S, &N) => return true,
+		(&NE, &SW) => return true,
+		(&SW, &NE) => return true,
+		(&NW, &SE) => return true,
+		(&SE, &NW) => return true,
+		(_, _) => return false,
+	}
+}
+
 #[derive(Debug)]
 enum Direction {
 	N,
@@ -33,4 +62,56 @@ enum Direction {
 	S,
 	SE,
 	SW,
+}
+
+#[derive(Debug)]
+enum ComboDirection {
+	Cancel,
+	Combo(Direction),
+	NoCombo,
+}
+
+impl Direction {
+	fn isNorth(&self) -> bool {
+		match self {
+			&N | &NE | &NW => true,
+			_ => false,
+		}
+	}
+
+	fn isSouth(&self) -> bool {
+		match self {
+			&S | &SE | &SW => true,
+			_ => false,
+		}
+	}
+
+	fn isEast(&self) -> bool {
+		match self {
+			&NE | &SE => true,
+			_ => false,
+		}
+	}
+
+	fn isWest(&self) -> bool {
+		match self {
+			&NW | &SW => true,
+			_ => false,
+		}
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn are_opposite_true() {
+		assert_eq!(areOpposite(&N,&S),true);
+	}
+
+	#[test]
+	fn are_opposite_false() {
+		assert_eq!(areOpposite(&NE,&S),false);
+	}
 }
