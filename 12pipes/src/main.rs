3752b21c8386754b8626709e7d67d3d5e4fd80bc
Jo <vechema@gmail.com>
2018-03-02 07:41:26 -0600

    Read input for day 12 pipes part 1

--- 12pipes/src/main.rs
+++ 12pipes/src/main.rs
@@ -1,5 +1,36 @@
 extern crate utilities;
 fn main() {
     let input = utilities::read_file("input.txt");
-	println!("{}", input);
+	let edge_list = format_input(&input);
+	println!("{:?}",edge_list);
+	let group = find_group(0, &edge_list);
+}
+
+fn format_input(input: &String) -> Vec<Vec<i32>> {
+	let mut result : Vec<Vec<i32>> = Vec::with_capacity(2000);
+	for line in input.lines() {
+		let mut parts = line.split(" ");
+		let index = parts.next().unwrap().parse::<usize>().unwrap();
+
+		// Get rid of <->
+		parts.next();
+
+		let kids = {
+			parts.map(|x| { // &str
+					if x.contains(",") {
+						(&x[..x.len()-1]).parse::<i32>().unwrap()
+					} else {
+						x.parse::<i32>().unwrap()
+					}
+				})
+			.collect::<Vec<i32>>()
+		};
+		result.push(kids);
+	}
+
+	result
+}
+
+fn find_group(member: usize, edge_list: &Vec<Vec<i32>>) -> Vec<i32>{
+	Vec::new()
 }
