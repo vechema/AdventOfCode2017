c895f64e9af6201133756aa055958c6c2ba0cede
Jo <vechema@gmail.com>
2018-03-05 08:09:24 -0600

    Solve day 12 pipes part 1

--- 12pipes/src/main.rs
+++ 12pipes/src/main.rs
@@ -1,13 +1,15 @@
 extern crate utilities;
+
 fn main() {
     let input = utilities::read_file("input.txt");
 	let edge_list = format_input(&input);
-	println!("{:?}",edge_list);
-	let group = find_group(0, &edge_list);
+	let group_member = 0;
+	let group = find_group(group_member, &edge_list);
+	println!("Group for {}, {:?}. Length: {}",group_member, group, group.len());
 }
 
-fn format_input(input: &String) -> Vec<Vec<i32>> {
-	let mut result : Vec<Vec<i32>> = Vec::with_capacity(2000);
+fn format_input(input: &String) -> Vec<Vec<usize>> {
+	let mut result : Vec<Vec<usize>> = Vec::with_capacity(2000);
 	for line in input.lines() {
 		let mut parts = line.split(" ");
 		let index = parts.next().unwrap().parse::<usize>().unwrap();
@@ -18,12 +20,12 @@ fn format_input(input: &String) -> Vec<Vec<i32>> {
 		let kids = {
 			parts.map(|x| { // &str
 					if x.contains(",") {
-						(&x[..x.len()-1]).parse::<i32>().unwrap()
+						(&x[..x.len()-1]).parse::<usize>().unwrap()
 					} else {
-						x.parse::<i32>().unwrap()
+						x.parse::<usize>().unwrap()
 					}
 				})
-			.collect::<Vec<i32>>()
+			.collect::<Vec<usize>>()
 		};
 		result.push(kids);
 	}
@@ -31,6 +33,19 @@ fn format_input(input: &String) -> Vec<Vec<i32>> {
 	result
 }
 
-fn find_group(member: usize, edge_list: &Vec<Vec<i32>>) -> Vec<i32>{
-	Vec::new()
+fn find_group(member: usize, edge_list: &Vec<Vec<usize>>) -> Vec<usize>{
+	let mut index_list : Vec<usize> = vec![member];
+	let mut used_indices = Vec::new();
+
+	while !index_list.is_empty() {
+		let index = index_list.pop().unwrap();
+		if !used_indices.contains(&index) {
+			used_indices.push(index);
+
+			// Add all the elements of index to index_list
+			let mut useable_edges = edge_list[index].clone();
+			index_list.append(&mut useable_edges);
+		}
+	}
+	used_indices
 }
